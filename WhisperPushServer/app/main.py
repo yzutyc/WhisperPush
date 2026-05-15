7f38868ed0c90679481f1ac1acc680d4a0d0319d
yucheng <yucheng@email.cn>
2026-05-15 14:45:46 +0800

    chore: bump to 0.1.5

--- WhisperPushServer/app/main.py
+++ WhisperPushServer/app/main.py
@@ -6,7 +6,7 @@ from starlette.requests import Request
 from app.middleware.logging_middleware import RequestLoggingMiddleware
 from app.routers import auth, secrets, messages, devices, two_factor, user_settings
 
-app = FastAPI(title="WhisperPush API", version="0.1.4")
+app = FastAPI(title="WhisperPush API", version="0.1.5")
 
 class DynamicCORSMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
